c35f9f515094a5421d01385e42a41ae0be53634a
ashtewari <ash.tewari.name@gmail.com>
2012-02-26 15:57:44 -0500

    nonword check

--- week1.rb
+++ week1.rb
@@ -3,8 +3,15 @@ class Hw1
 	def initialize
 	end
 
-	def palindrome?(string)		
-		string.downcase == string.downcase.reverse
+	def palindrome?(string) 
+    
+    if(string == nil || string.length == 0) 
+      return false 
+    end
+    
+    input = string.downcase.gsub(/\W/, '')
+		input == input.reverse
+    
 	end
 end
 
@@ -26,27 +33,40 @@ class PalindromeTest < Test::Unit::TestCase
 	end
 
 	def test_palindrome_invalid()
-		target = Hw1.new
 
-		input = 'abc'		
-		actual = target.palindrome?(input)
-		assert(!actual)
+    target = Hw1.new
+		
+    assert(!target.palindrome?(''))
+    assert(!target.palindrome?(nil))
 
 	end
 
 	def test_palindrome_simple_valid()
 		target = Hw1.new
 
-		input = 'aaa'		
-		assert(target.palindrome?(input))
+    assert(!target.palindrome?('abc'))
+		assert(target.palindrome?('aaa'))
+    assert(target.palindrome?(' '))
+    assert(target.palindrome?(' !'))    
 
 	end
 
 	def test_palindrome_simple_cased_valid()
 		target = Hw1.new
+	
+		assert(target.palindrome?("Aaa"))
 
-		input = 'Aaa'		
-		assert(target.palindrome?(input))
+	end
+
+	def test_palindrome_simple_nonword_valid()
+		target = Hw1.new
+
+		assert(target.palindrome?("Aa!a"))
+    assert(target.palindrome?("A a!!a"))
+    assert(target.palindrome?("A man, a plan, a canal -- Panama"))
+    assert(target.palindrome?("Madam, I'm Adam!"))
+    assert(!target.palindrome?("Abracadabra"))
+
 
 	end
 end
